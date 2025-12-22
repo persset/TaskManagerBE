7db237ee7032acb6e56ae72afffefd1a8f17efb7
Jose Victor <victor.persset@gmail.com>
2025-12-22 11:24:23 -0300

    feat: add automapper dependency

--- TaskManagerBE/obj/Debug/net10.0/TaskManagerBE.AssemblyInfo.cs
+++ TaskManagerBE/obj/Debug/net10.0/TaskManagerBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc44fec632c192f8f41aa021fad40fd9b67bfc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a53c3fd07483d585f0baf52fdc7ccb9490116b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
