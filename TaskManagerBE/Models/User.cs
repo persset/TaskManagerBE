c18a69b3d217566a2a8d8cb6ac39b6814f2aef5d
Jose Victor <victor.persset@gmail.com>
2025-12-22 12:40:22 -0300

    feat: remove username attribute from user model

--- TaskManagerBE/Models/User.cs
+++ TaskManagerBE/Models/User.cs
@@ -3,7 +3,6 @@ namespace TaskManagerBE.Models
     public class User : BaseModel
     {
         public string Name { get; set; } = "";
-        public string Username { get; set; } = "";
         public string Email { get; set; } = "";
         public string Password { get; set; } = "";
     }
