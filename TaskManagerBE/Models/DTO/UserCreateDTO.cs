17fa2012df82c445d870d67cabbcd2bdbe3673b7
Jose Victor <victor.persset@gmail.com>
2025-12-22 12:43:12 -0300

    feat: create UserCreateDTO

---
+++ TaskManagerBE/Models/DTO/UserCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace TaskManagerBE.Models
+{
+    public class UserCreateDTO
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
--- TaskManagerBE/Models/DTO/UserResponseDTO.cs
+++ TaskManagerBE/Models/DTO/UserResponseDTO.cs
@@ -1,10 +1,9 @@
 namespace TaskManagerBE.Models
 {
-    public class UserDTO
+    public class UserResponseDTO
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
-        public string Username { get; set; } = "";
         public string Email { get; set; } = "";
     }
 }
