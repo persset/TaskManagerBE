9cc66cb54a878ef160917a616d6b64d6f52fca10
Jose Victor <victor.persset@gmail.com>
2026-01-12 15:33:20 -0300

    feat: create UserService classes

---
+++ TaskManagerBE/Services/User/UserService.cs
@@ -0,0 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerBE.Data;
+
+namespace TaskManagerBE.Services.User
+{
+    public class UserService : IUserService
+    {
+        private readonly DataContext dataContext;
+
+        public UserService(DataContext context)
+        {
+            dataContext = context;
+        }
+
+        public async Task<Models.User> Create(Models.User entity)
+        {
+            dataContext.Users.Add(entity);
+
+            await dataContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<Models.User?> Delete(int id)
+        {
+            var user = await dataContext.Users.FindAsync(id);
+
+            if (user == null)
+                return null;
+
+            dataContext.Users.Remove(user);
+
+            await dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<List<Models.User>> GetAll()
+        {
+            return await dataContext.Users.ToListAsync();
+        }
+
+        public async Task<Models.User?> GetSingle(int id)
+        {
+            var user = await dataContext.Users.FindAsync(id);
+
+            if (user == null)
+                return null;
+
+            return user;
+        }
+
+        public async Task<Models.User?> Update(int id, Models.User entity)
+        {
+            var user = await dataContext.Users.FindAsync(id);
+
+            if (user == null)
+                return null;
+
+            user.Name = entity.Name;
+            user.Email = entity.Email;
+
+            await dataContext.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
